be4c3074ab13f3349fd21e8af67ad87ad2993c5d
Damien Coney <d.coney@kainos.com>
2015-06-16 11:38:50 +0100

    Initial commit of the skeleton client

---
+++ src/main/java/com/kainos/training/jersey/client/BaseClient.java
@@ -0,0 +1,18 @@
+package com.kainos.training.jersey.client;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.Response;
+
+public class BaseClient 
+{
+
+	public void getLogin(String username, String password){
+		//Code goes here....		
+	}
+}
